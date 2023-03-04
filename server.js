2be2e2ad28749e3ef5b31269cd5d509fe9d1e4ad
kstunger <kstunger@users.noreply.github.com>
2023-03-04 09:47:12 -0800

    Add request timeout

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const url = require('url');
 
 // Create http server.
 let server = http.createServer((req, res) => {
-  if(req.method == "POST") {
+  if (req.method == "POST") {
     var postData = '';
     req.on('data', function (chunk) {
       postData += chunk;
     });
-    
+
     req.on('end', function () {
       console.log(req.socket.remoteAddress + ": " + postData);
+    });
+
+    req.setTimeout(10000, function() {
+      console.error('Request timed out.');
+      req.abort();
+    });
+
+    req.on('error', function (err) {
+      console.error(err);
+      req.abort();
     });
   }
   res.writeHead(200);
